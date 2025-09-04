cfaaa9c22dbb97da08b43c9af4131061aca50746
Raghav123-30 <raghavendrabhat2018@gmail.com>
2025-09-04 19:03:46 +0530

    Setup tailwindcss and motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { motion } from "motion/react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function Home() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="h-screen w-full flex flex-col items-center justify-center bg-neutral-900">
+      <motion.button
+        initial={{
+          opacity: 0,
+          x: "-100%",
+        }}
+        animate={{
+          opacity: 1,
+          x: 0,
+        }}
+        transition={{
+          duration: 0.5,
+        }}
+        className="text-neutral-400 px-12 py-4 rounded-lg bg-black  shadow-md cursor-pointer"
+      >
+        Lets get started
+      </motion.button>
+    </div>
+  );
 }
-
-export default App
